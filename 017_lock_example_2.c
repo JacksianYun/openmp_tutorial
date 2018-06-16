7460f0e4340458d66d233718475080e543822f03
JacksianYun <networkprof09@gmail.com>
2018-06-16 04:09:25 -0700

    add 017_lock_example_2.c

--- 017_lock_example_2.c
+++ 017_lock_example_2.c
@@ -2,3 +2,25 @@
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
+
+omp_lock_t simple_lock;
+
+int main() {
+	omp_init_lock(&simple_lock);
+
+	#pragma omp parallel num_threads(4)
+	{
+		int tid = omp_get_thread_num();
+
+		while (!omp_test_lock(&simple_lock))
+			printf("Thread %d - failed to acquire simple_lock\n", tid);
+
+		printf("Thread %d - acquired simple_lock\n", tid);
+
+		printf("Thread %d - release simple_lock\n", tid);
+
+		omp_unset_lock(&simple_lock);
+		
+	}
+	omp_destroy_lock(&simple_lock);
+}
